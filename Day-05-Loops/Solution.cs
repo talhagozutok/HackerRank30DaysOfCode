356634e55c3801d7453a27c004e316cbb773526e
talhagozutok <talhagztk@gmail.com>
2023-09-15 16:40:12 +0300

    completed: Day-05 tutorial

---
+++ Day-05-Loops/Solution.cs
@@ -0,0 +1,14 @@
+﻿namespace Day_05_Loops;
+
+internal class Solution
+{
+    static void Main(string[] args)
+    {
+        int n = Convert.ToInt32(Console.ReadLine().Trim());
+
+        for (int i = 1; i <= 10; i++)
+        {
+            Console.WriteLine($"{n} x {i} = {n * i}");
+        }
+    }
+}
