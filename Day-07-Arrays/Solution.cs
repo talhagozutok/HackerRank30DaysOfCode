3f8d259c55c06b83fad288d1f36c41247d1deaec
talhagozutok <talhagztk@gmail.com>
2023-09-17 22:35:02 +0300

    completed: Day-07 tutorial

---
+++ Day-07-Arrays/Solution.cs
@@ -0,0 +1,18 @@
+﻿class Solution
+{
+    public static void Main(string[] args)
+    {
+        int n = Convert.ToInt32(Console.ReadLine().Trim());
+
+        List<int> arr = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(arrTemp => Convert.ToInt32(arrTemp)).ToList();
+
+        Console.WriteLine(GetReverseArray(arr));
+    }
+
+    public static string GetReverseArray(List<int> arr)
+    {
+        arr.Reverse();
+        string result = string.Join(' ', arr);
+        return result;
+    }
+}
