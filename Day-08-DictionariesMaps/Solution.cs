34b036739bdfb33ac6b7edc42adcaaee3cca2a03
talhagozutok <talhagztk@gmail.com>
2023-09-18 15:37:05 +0300

    completed: Day-08 tutorial

---
+++ Day-08-DictionariesMaps/Solution.cs
@@ -0,0 +1,57 @@
+﻿namespace Day_08_DictionariesMaps;
+
+internal class Solution
+{
+    static void Main(string[] args)
+    {
+        int n = int.Parse(Console.ReadLine().Trim());
+
+        Dictionary<string, string> phoneBook = new();
+
+        for (int i = 0; i < n; i++)
+        {
+            string[] phoneBookRecord = Console.ReadLine()
+                .TrimEnd()
+                .Split(' ');
+
+            phoneBook.Add(
+                phoneBookRecord[0],
+                phoneBookRecord[1]);
+        }
+
+        string phoneOwnerInput = string.Empty;
+        while ((phoneOwnerInput = Console.ReadLine()) != null)
+        {
+            ShowPhoneNumber(phoneBook, phoneOwnerInput);
+        }
+    }
+
+    static string GetPhoneNumber(
+        Dictionary<string, string> phoneBook,
+        string phoneOwner)
+    {
+        phoneBook.TryGetValue(
+            phoneOwner,
+            out string? phoneNumber);
+
+        return phoneNumber;
+    }
+
+    static void ShowPhoneNumber(
+        Dictionary<string, string> phoneBook,
+        string phoneOwner)
+    {
+        string phoneNumber = GetPhoneNumber(
+                phoneBook,
+                phoneOwner);
+
+        if (phoneNumber is null)
+        {
+            Console.WriteLine("Not found");
+        }
+        else
+        {
+            Console.WriteLine($"{phoneOwner}={phoneNumber}");
+        }
+    }
+}
