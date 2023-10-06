6f27c01476128ccce2c95b19e4409b75bbd5eebd
talhagozutok <talhagztk@gmail.com>
2023-10-06 19:33:26 +0300

    completed: Day-09 tutorial

---
+++ Day-09-Recursion/Solution.cs
@@ -0,0 +1,25 @@
+﻿namespace Day_09_Recursion;
+class Result
+{
+    public static int Factorial(int n)
+    {
+        if (n <= 1)
+        {
+            return 1;
+        }
+
+        return n * Factorial(n - 1);
+    }
+}
+
+internal class Solution
+{
+    static void Main(string[] args)
+    {
+        int n = Convert.ToInt32(Console.ReadLine().Trim());
+
+        int result = Result.Factorial(n);
+
+        Console.WriteLine(result);
+    }
+}
