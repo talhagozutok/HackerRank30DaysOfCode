4f6b6140bb4398ab5900ba147ed446ade60d9859
talhagozutok <talhagztk@gmail.com>
2023-10-08 14:11:53 +0300

    completed: Day-13 tutorial

---
+++ Day-13-AbstractClasses/Solution.cs
@@ -0,0 +1,43 @@
+﻿namespace Day_13_AbstractClasses;
+
+abstract class Book
+{
+    protected String title;
+    protected String author;
+
+    public Book(String t, String a)
+    {
+        title = t;
+        author = a;
+    }
+    public abstract void display();
+}
+
+class MyBook : Book
+{
+    public int price { get; set; }
+    public MyBook(string title, string author, int price)
+        : base(title, author)
+    {
+        this.price = price;
+    }
+
+    public override void display()
+    {
+        Console.WriteLine($"Title: {title}");
+        Console.WriteLine($"Author: {author}");
+        Console.WriteLine($"Price: {price}");
+    }
+}
+
+internal class Solution
+{
+    static void Main(string[] args)
+    {
+        String title = Console.ReadLine();
+        String author = Console.ReadLine();
+        int price = Int32.Parse(Console.ReadLine());
+        Book new_novel = new MyBook(title, author, price);
+        new_novel.display();
+    }
+}
