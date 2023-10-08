d8bb357c512be2772c9ddf9ed6b18a85e16b5857
talhagozutok <talhagztk@gmail.com>
2023-10-08 22:44:48 +0300

    completed: Day-17 tutorial

---
+++ Day-17-MoreExceptions/Solution.cs
@@ -0,0 +1,39 @@
+﻿namespace Day_17_MoreExceptions;
+
+class Calculator
+{
+    public int power(int n, int p)
+    {
+        if (n < 0 || p < 0)
+        {
+            throw new Exception("n and p should be non-negative");
+        }
+
+        return (int)Math.Pow(n, p);
+    }
+}
+
+internal class Solution
+{
+    static void Main(string[] args)
+    {
+        Calculator myCalculator = new Calculator();
+        int T = Int32.Parse(Console.ReadLine());
+        while (T-- > 0)
+        {
+            string[] num = Console.ReadLine().Split();
+            int n = int.Parse(num[0]);
+            int p = int.Parse(num[1]);
+            try
+            {
+                int ans = myCalculator.power(n, p);
+                Console.WriteLine(ans);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+
+            }
+        }
+    }
+}
