19d7feb60544fbf4ef9df820487b79f6b4f4aaa7
talhagozutok <talhagztk@gmail.com>
2023-10-08 20:02:48 +0300

    completed: Day-14 tutorial

---
+++ Day-14-Scope/Solution.cs
@@ -0,0 +1,38 @@
+﻿namespace Day_14_Scope;
+
+class Difference
+{
+    private int[] elements;
+    public int maximumDifference;
+
+    // Add your code here
+    public Difference(int[] arr)
+    {
+        elements = arr;
+    }
+
+    public void computeDifference()
+    {
+        int max = elements.Max();
+        int min = elements.Min();
+
+        maximumDifference = Math.Abs(max - min);
+    }
+
+} // End of Difference Class
+
+internal class Solution
+{
+    static void Main(string[] args)
+    {
+        Convert.ToInt32(Console.ReadLine());
+
+        int[] a = Console.ReadLine().Split(' ').Select(x => Convert.ToInt32(x)).ToArray();
+
+        Difference d = new Difference(a);
+
+        d.computeDifference();
+
+        Console.Write(d.maximumDifference);
+    }
+}
