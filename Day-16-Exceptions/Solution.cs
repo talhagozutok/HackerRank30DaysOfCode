1d7e4c8094898369139ad7957a5226f3e1c442c3
talhagozutok <talhagztk@gmail.com>
2023-10-08 22:26:09 +0300

    completed: Day-16 tutorial

---
+++ Day-16-Exceptions/Solution.cs
@@ -0,0 +1,18 @@
+﻿namespace Day_16_Exceptions;
+
+internal class Solution
+{
+    static void Main(string[] args)
+    {
+        string S = Console.ReadLine();
+
+        try
+        {
+            Console.WriteLine(int.Parse(S));
+        }
+        catch
+        {
+            Console.WriteLine("Bad String");
+        }
+    }
+}
