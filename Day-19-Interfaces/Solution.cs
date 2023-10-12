711bce3896ee4c193f6a50bc440be26cf7c1c3c9
talhagozutok <talhagztk@gmail.com>
2023-10-12 22:45:27 +0300

    completed: Day-19 tutorial

---
+++ Day-19-Interfaces/Solution.cs
@@ -0,0 +1,35 @@
+﻿namespace Day_19_Interfaces;
+
+public interface AdvancedArithmetic
+{
+    int divisorSum(int n);
+}
+
+public class Calculator : AdvancedArithmetic
+{
+    public int divisorSum(int n)
+    {
+        int sum = 1;
+
+        for (int i = 2; i <= n; i++)
+        {
+            if (n % i == 0)
+            {
+                sum += i;
+            }
+        }
+
+        return sum;
+    }
+}
+
+internal class Solution
+{
+    static void Main(string[] args)
+    {
+        int n = Int32.Parse(Console.ReadLine());
+        AdvancedArithmetic myCalculator = new Calculator();
+        int sum = myCalculator.divisorSum(n);
+        Console.WriteLine("I implemented: AdvancedArithmetic\n" + sum);
+    }
+}
