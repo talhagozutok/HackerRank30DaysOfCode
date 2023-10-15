27441c332ba12ddab8c7ae5790937dda7cc74aa1
talhagozutok <talhagztk@gmail.com>
2023-10-16 00:08:56 +0300

    completed: Day-29 tutorial

---
+++ Day-29-BitwiseAnd/Solution.cs
@@ -0,0 +1,23 @@
+﻿namespace Day_29_BitwiseAnd;
+
+class Result
+{
+    public static int bitwiseAnd(int N, int K) => (K - 1 | K) <= N ? K - 1 : K - 2;
+}
+
+internal class Solution
+{
+    static void Main(string[] args)
+    {
+        int t = Convert.ToInt32(Console.ReadLine().Trim());
+
+        for (int tItr = 0; tItr < t; tItr++)
+        {
+            string[] firstMultipleInput = Console.ReadLine().TrimEnd().Split(' ');
+            int count = Convert.ToInt32(firstMultipleInput[0]);
+            int lim = Convert.ToInt32(firstMultipleInput[1]);
+            int res = Result.bitwiseAnd(count, lim);
+            Console.WriteLine(res);
+        }
+    }
+}
